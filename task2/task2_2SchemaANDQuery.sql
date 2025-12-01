28c512ac235b3a6f3f308f6848cb9c6dd506154c
AhmedAA3 <ahmed111adam@outlook.com>
2025-12-01 14:03:40 +0100

    Update task2_2SchemaANDQuery.sql

--- task2/task2_2SchemaANDQuery.sql
+++ task2/task2_2SchemaANDQuery.sql
@@ -1,164 +1,227 @@
+/* DROP VIEW IF EXISTS relation; */
+DROP TRIGGER IF EXISTS alt ON allocation;
+DROP TABLE IF EXISTS allocation;
+DROP TABLE IF EXISTS planned_activity;
+DROP TABLE IF EXISTS course_instance;
+DROP TABLE IF EXISTS teaching_activity;
+DROP TABLE IF EXISTS salary_history;
+DROP TABLE IF EXISTS skill_set;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS course_layout;
+DROP TABLE IF EXISTS job_title;
+DROP TABLE IF EXISTS person;
 
 
-INSERT INTO person (personnummer, first_name, last_name, phone_number, address)
-VALUES
-  ('850716-9541','Unity','Aline','050-954-2518','571-1803 Ut, Street'),
-  ('690515-4305','Vincent','Ila','012-637-2222','Ap #443-5429 Libero St.'),
-  ('040201-1234','Alexa','Jin','042-219-4453','5286 Aliquet Street'),
-  ('760216-5552','Hayley','Eleanor','024-883-7724','140-8122 Euismod St.'),
-  ('760216-5552','Marcus','Adams','071-284-5964','671 Non St.');
+/* instnance per period needs triggers so that an employee cant have more then */
+CREATE TABLE person (
+  id SERIAL PRIMARY KEY,
+  personnummer CHAR(20) NOT NULL,
+  first_name CHAR(20) NOT NULL,
+  last_name CHAR(20) NOT NULL,
+  phone_number CHAR(15),
+  address CHAR(30)
+);
 
 
-
-INSERT INTO job_title (job_title)
-VALUES
-  ('Library-management'),
-  ('Principal'),
-  ('Professor');
+CREATE TABLE job_title (
+  job_title CHAR(40) NOT NULL PRIMARY KEY
+);
 
 
-
-INSERT INTO department (department_name, person_id)
-VALUES
-  ('Department-of-Chemistry', 5),
-  ('Department-of-Physics', 5),
-  ('Department-of-Biology', 5),
-  ('Department-of-IT', 5),
-  ('Department-of-Math', 5);
+CREATE TABLE department (
+  department_id SERIAL NOT NULL PRIMARY KEY,
+  department_name CHAR(40) NOT NULL,
+  person_id INT NOT NULL REFERENCES person(id)
+);
 
 
-
-INSERT INTO employee (person_id, salary, employee_id, supervisor_id, department_id, job_title)
-VALUES
-  (1, 34532, 1418, 1, 4, 'Library-management'),
-  (2, 35194, 1656, 2, 5, 'Professor'),
-  (3, 32338, 1835, 3, 1, 'Professor'),
-  (4, 38639, 1296, 5, 2, 'Professor'),
-  (5, 47559, 1588, NULL, 3, 'Principal');
-
-
+CREATE TABLE employee (
+  person_id INT NOT NULL PRIMARY KEY REFERENCES person(id),
+  salary INT NOT NULL,
+  employee_id INT NOT NULL,
+  supervisor_id INT REFERENCES person(id),
+  department_id INT NOT NULL REFERENCES department(department_id),
+  job_title CHAR(40) NOT NULL REFERENCES job_title(job_title)
+);
 
 
-INSERT INTO skill_set (person_id, skill)
-VALUES
-    (1, 'Communications'),
-    (1, 'Administration'),
-
-    (2, 'Mathematics'),
-    (2, 'Programming'),
-
-    (3, 'Physics'),
-    (3, 'Lab Work'),
-
-    (4, 'Chemistry'),
-    (4, 'Safety'),
-
-    (5, 'Leadership'),
-    (5, 'Management'),
-    (5, 'Budgeting'),
-	(5, 'Communications');
+CREATE TABLE skill_set (
+	skill CHAR(15) NOT NULL,
+	person_id INT NOT NULL REFERENCES employee(person_id),
+	PRIMARY KEY (skill , person_id)
+);
 
 
 
-
-INSERT INTO salary_history (person_id, salary, valid_from, valid_to)
-VALUES
-  (5, 20000, '2023-01-01', '2023-12-31'),
-  (5, 22000, '2024-01-01', '2024-12-31'),
-  (5, 25000, '2025-01-01', NULL);
-
-
-INSERT INTO course_layout (course_code, course_name, min_student, max_student, hp, version_id)
-VALUES
-  ('SF3642','Topologi',50,80,7.5,1),
-  ('SF3641','Matematisk-statistik',45,170,7.5,1),
-  ('SF2621','Fourier-och-Laplace-transform',50,115,7.5,1),
-  ('SF1626','Partiella-differentialekvationer',50,170,7.5,1),
-  ('SF3646','Differentialekvationer',55,150,7.5, 1),
-  ('SF3642', 'Topologi', 75, 100, 15, 2);
-/* */
+CREATE TABLE salary_history (
+ person_id INT NOT NULL REFERENCES employee(person_id),
+ salary INT NOT NULL, 
+ valid_from DATE NOT NULL,
+ valid_to DATE,
+ PRIMARY KEY (person_id, valid_from)
+ );
 
 
 
 
-INSERT INTO course_instance (id, num_students, course_code, study_period, study_year, version_id)
-VALUES
-  (1, 71,'SF3642','P1',2025,1),
-  (2,161,'SF3642','P2',2025,2),
-  (3,57,'SF2621','P3',2025,1),
-  (4,69,'SF1626','P4',2025,1),
-  (5,107,'SF3646','P1',2026,1);
-/* */
+CREATE TABLE course_layout (
+  course_code CHAR(10) NOT NULL,
+  version_id INT NOT NULL,
+  course_name CHAR(50) NOT NULL,
+  min_student INT NOT NULL,
+  hp DECIMAL(3,1) NOT NULL,
+  max_student INT NOT NULL,
+  PRIMARY KEY (course_code, version_id)
+);
 
 
 
 
-
-INSERT INTO teaching_activity (activity_name, factor)
-VALUES
-  ('Lecture',6),
-  ('Tutorial',2),
-  ('Lab',2),
-  ('Seminar',2),
-  ('Other',2),
-  ('Admin',1),
-  ('Exam', 1);
-/* */
-
-
-
-INSERT INTO planned_activity (planned_hours, activity_id, course_instance_id) VALUES
-  (95, 1, 1),
-  (20,  2, 1),
-  (70, 3, 1),
-  (30, 4, 1),
-  (20, 5, 1),
-  (0, 6, 1),
-  (0, 7, 1),
-
-  (110, 1, 2),
-  (60,  2, 2),
-  (0,   3, 2),
-  (45, 4, 2),
-  (28, 5, 2),
-
-  (30, 1, 3),
-  (95, 2, 3),
-  (40, 3, 3),
-  (0,  4, 3),
-  (18, 5, 3),
-  (0, 6, 3),
-  (0, 7, 3),
-
-  (130, 1, 4),
-  (0,   2, 4),
-  (0,   3, 4),
-  (75,  4, 4),
-  (25,  5, 4),
-
-  (100, 1, 5),
-  (55,  2, 5),
-  (90,  3, 5),
-  (0,   4, 5),
-  (30,  5, 5);
+CREATE TABLE course_instance (
+  id           INT PRIMARY KEY,
+  num_students INT NOT NULL,
+  study_period CHAR(10) NOT NULL,
+  study_year   INT NOT NULL,
+  course_code  CHAR(10) NOT NULL,
+  version_id   INT NOT NULL,
+  FOREIGN KEY (course_code, version_id)
+    REFERENCES course_layout(course_code, version_id)
+);
 
 
 
 
 
-INSERT INTO allocation VALUES (1, 3, 1, 30); /* lecture*/
-INSERT INTO allocation VALUES (2, 3, 1, 45); /* tut*/
-INSERT INTO allocation VALUES (4, 3, 1, 20); /* lab*/
+CREATE TABLE teaching_activity (
+  id            SERIAL NOT NULL PRIMARY KEY,
+  activity_name CHAR(40) NOT NULL,
+  factor        INTEGER NOT NULL
+);
 
 
-INSERT INTO allocation VALUES (2, 3, 2, 50); /* tut*/  
-INSERT INTO allocation VALUES (4, 3, 3, 20); /* lab*/
-INSERT INTO allocation VALUES (5, 3, 4, 18); /* lab*/
-
-INSERT INTO allocation VALUES (6, 3, 4, 18); /* Admin*/
-INSERT INTO allocation VALUES (6, 3, 5, 18); /* Admin*/
-INSERT INTO allocation VALUES (7, 3, 5, 18); /* Exam*/
-INSERT INTO allocation VALUES (7, 3, 2, 18); /* Exam*/
+CREATE TABLE planned_activity (
+  activity_id       INTEGER NOT NULL REFERENCES teaching_activity(id),
+  course_instance_id INTEGER NOT NULL REFERENCES course_instance(id),
+  planned_hours      INT NOT NULL,
+  PRIMARY KEY (activity_id, course_instance_id)
+);
 
 
+CREATE TABLE allocation (
+   activity_id        INT NOT NULL REFERENCES teaching_activity(id),
+   course_instance_id INT NOT NULL REFERENCES course_instance(id),
+   person_id          INT NOT NULL REFERENCES employee(person_id),
+   allocation_hours   INT NOT NULL DEFAULT 0,
+   PRIMARY KEY (activity_id, course_instance_id, person_id)
+);
 
+CREATE OR REPLACE FUNCTION checkemployeelimit()
+RETURNS TRIGGER AS $$
+DECLARE
+    instance_count INT;
+    new_period CHAR(10);
+    new_year INT;
+    already_has_course BOOLEAN;
+BEGIN
+    SELECT study_period, study_year
+    INTO new_period, new_year
+    FROM course_instance
+    WHERE id = NEW.course_instance_id;
+
+    SELECT EXISTS (
+        SELECT 1 
+        FROM allocation 
+        WHERE person_id = NEW.person_id
+          AND course_instance_id = NEW.course_instance_id
+    )
+    INTO already_has_course;
+
+	
+    SELECT COUNT(DISTINCT ci.id)
+     INTO instance_count
+	 FROM allocation a
+	 JOIN course_instance ci ON ci.id = a.course_instance_id
+	 WHERE a.person_id = NEW.person_id
+ 	 AND ci.study_period = new_period;  
+
+
+    IF NOT already_has_course AND instance_count >= 4 THEN
+        RAISE EXCEPTION 
+            'Teacher % cannot have more than 4 courses in period % year %',
+            NEW.person_id, new_period, new_year;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+DROP TRIGGER IF EXISTS alt ON allocation;
+
+CREATE TRIGGER alt
+BEFORE INSERT ON allocation
+FOR EACH ROW
+EXECUTE FUNCTION checkemployeelimit();
+
+
+
+SELECT
+    ci.course_code AS "Course Code",
+    pa.course_instance_id AS "Course Instance ID",
+	cl.hp AS "HP",
+	p.first_name AS "Teacher's first name",
+	e.job_title AS "Designation",
+   	
+	SUM(CASE WHEN ta.activity_name = 'Lecture' THEN a.allocation_hours * ta.factor ELSE 0 END )
+    		AS "Lecture Hours",
+	SUM(CASE WHEN ta.activity_name = 'Tutorial' THEN a.allocation_hours * ta.factor ELSE 0 END )
+    		AS "Tutorial Hours",
+	SUM(CASE WHEN ta.activity_name = 'Lab' THEN a.allocation_hours * ta.factor ELSE 0 END )
+    		AS "Lab Hours",
+	SUM(CASE WHEN ta.activity_name = 'Seminar' THEN a.allocation_hours * ta.factor ELSE 0 END )
+    		AS "Seminar Hours",
+	SUM(CASE WHEN ta.activity_name = 'Other' THEN a.allocation_hours * ta.factor ELSE 0 END )
+    		AS "Other Overhead Hours",
+	SUM(CASE WHEN ta.activity_name = 'Admin' THEN (2 * cl.hp + 0.2 * ci.num_students + 28) ELSE 0 END)
+		 AS  "Admin",
+	SUM(CASE WHEN ta.activity_name = 'Exam' THEN 32 + (0.725 * ci.num_students) ELSE 0 END)
+		AS "Exam",
+	(	
+		SUM(CASE WHEN ta.activity_name = 'Lecture' THEN a.allocation_hours * ta.factor ELSE 0 END ) + 
+		SUM(CASE WHEN ta.activity_name = 'Tutorial'  THEN a.allocation_hours * ta.factor ELSE 0 END )+ 
+		SUM(CASE WHEN ta.activity_name = 'Lab' THEN a.allocation_hours * ta.factor ELSE 0 END )+ 
+		SUM(CASE WHEN ta.activity_name = 'Seminar' THEN a.allocation_hours * ta.factor ELSE 0 END )+ 
+		SUM(CASE WHEN ta.activity_name = 'Other' THEN a.allocation_hours * ta.factor ELSE 0 END ) +
+		SUM(CASE WHEN ta.activity_name = 'Admin'THEN (2 * cl.hp + 0.2 * ci.num_students + 28) ELSE 0 END) +
+		SUM(CASE WHEN ta.activity_name = 'Exam' THEN 32 + (0.725 * ci.num_students) ELSE 0 END)
+	) AS "Total Hours"	
+FROM allocation AS a, 
+	course_instance AS ci,  
+	course_layout AS cl, 
+	teaching_activity AS ta, 
+	planned_activity AS pa,
+	person AS p,
+	employee AS e
+	
+
+WHERE ci.id = a.course_instance_id 
+  AND cl.course_code = ci.course_code 
+  AND cl.version_id = ci.version_id  
+  AND ta.id = pa.activity_id 
+  AND pa.course_instance_id = ci.id
+  AND p.id = a.person_id
+  AND e.person_id = p.id               
+  AND ci.id = 3
+  AND a.person_id = p.id 
+  AND pa.course_instance_id  = a.course_instance_id 
+  AND pa.activity_id = a.activity_id	 
+ 
+GROUP BY 
+    ci.course_code,
+	pa.course_instance_id,
+    cl.hp,
+	p.first_name,
+	e.job_title,
+	ci.num_students
+ORDER BY p.first_name DESC;
